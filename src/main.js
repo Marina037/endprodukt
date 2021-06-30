0303af2504c2121370a61011b35f73d433e881f3
Marina037 <60391271+Marina037@users.noreply.github.com>
2021-06-30 16:33:52 +0200

    js added

--- src/main.js
+++ src/main.js
@@ -1,8 +1,63 @@
-import Vue from 'vue'
-import App from './App.vue'
+"use strict"
 
-Vue.config.productionTip = false
+// Diese Model Variablen an der richtigen Stelle einbinden
+/*
+ort: '',
+connections: []
+*/
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+// Diese Funktionen an der richtigen einbinden.
+/*
+loadConnections: function() {
+    const api = 'https://transport.opendata.ch/v1/stationboard?station=';
+    let url = api + this.ort;
+    fetch( url )
+        .then ( async response => await response.json() )
+        .then ( (antwort) => this.ausgabe(antwort))
+        .catch( fehler => alert(fehler) )
+},
+
+ausgabe: function(antwort) {
+    this.ort = antwort.station.name
+    this.connections = antwort.stationboard
+},
+
+timestamp2time: function(timestamp) {
+    let time = new Date(timestamp);
+    return time.toLocaleTimeString('de-CH');
+}
+*/
+
+const StationBoard = {
+    data: function() {
+        return {
+            ort: 'Romanshorn',
+            connections: [],
+        }
+    },
+
+    methods: {
+        loadConnections: function () {
+            const api = 'https://transport.opendata.ch/v1/stationboard?station=';
+            let url = api + this.ort;
+            fetch( url )
+                .then ( async response => await response.json() )
+                .then ( (antwort) => this.ausgabe(antwort))
+                .catch( fehler => alert(fehler) )
+        },
+
+        ausgabe(antwort) {
+            this.ort = antwort.station.name
+            this.connections = antwort.stationboard
+        },
+
+        timestamp2time(timestamp) {
+            let time = new Date(timestamp);
+            return time.toLocaleTimeString('de-CH');
+        }
+    }
+}
+
+const app = Vue.createApp(StationBoard);
+app.mount("#StationBoard"); 
+
